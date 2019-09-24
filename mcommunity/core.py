1b1306a3cb38f400f8e06507b9423c7ae5da1f72
Rob Carleski <carleski@umich.edu>
2019-09-24 12:14:51 -0400

    Revert "Merge branch 'csam_bugfixes' into 'master'"
    This reverts merge request !4

--- mcommunity/core.py
+++ mcommunity/core.py
@@ -53,7 +53,7 @@ def get_entity_dn(client, name):
                 if item['naming'].casefold() == name.casefold():
                     return item['dn']
             elif item['group']:
-                if item['displayName'].casefold() == name.casefold():
+                if item['displayName'].casefold() == name:
                     return item['dn']
                 else:
                     r = client.get('/profile/dn/{}'.format(
