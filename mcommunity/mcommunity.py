142695a050a5f7e18fb5636f34c09b7b1de76447
Rob Carleski <carleski@umich.edu>
2019-11-03 06:29:33 -0500

    Add cached data checks for update_membership
    WIthout these, update_membership() will nuke existing members, group
    members, and external members if they're not referenced in the parent
    code. Ope.

--- mcommunity/mcommunity.py
+++ mcommunity/mcommunity.py
@@ -549,10 +549,13 @@ class MCommGroup:
         None
         """
         if self.memberDn or self.members:
+            self.members
             self.update_members()
         if self.memberGroupDn or self.memberGroups:
+            self.memberGroups
             self.update_member_groups()
         if self.memberExternal or self.externalMembers:
+            self.externalMembers
             self.update_external_members()
 
     def update_members(self):
